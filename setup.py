1567373780b61b6f79402a4f03c21474ba7050b3
Proskuryakov Alesha <a.proskuryakov@propellerads.net>
2016-07-19 10:57:54 +0300

    requires in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+from pip.req import parse_requirements
 from setuptools import setup
+
+reqs = map(lambda x: str(x.req), parse_requirements("requirements.txt", session=False))
 
 setup(name='generators',
       version='0.1',
@@ -6,4 +9,5 @@ setup(name='generators',
       author='alexeyka',
       author_email='stack2008@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=reqs
       )
